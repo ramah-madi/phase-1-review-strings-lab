56c2e41d7daaeef0e7568193cb8b38c1ed9bc1e1
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-09-26 00:43:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'ramah madi'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
